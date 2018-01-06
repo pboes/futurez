696b7dd810705537be1c191050d5015f66e4da8d
Paul Boes <paulboes@Pauls-MacBook-Pro.local>
2018-01-07 00:12:19 +0100

    changed the redirect also for signup form

--- conf/views.py
+++ conf/views.py
@@ -15,7 +15,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return HttpResponseRedirect(reverse("polls:user_home"))
+            return HttpResponseRedirect(reverse("polls:poll"))
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
